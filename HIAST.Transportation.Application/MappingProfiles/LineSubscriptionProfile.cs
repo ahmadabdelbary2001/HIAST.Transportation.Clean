4fd9eec3731b4bf5c39c94a53770279b28b74675
ahmadabdelbary2001 <ahmadabdelbary2001@gmail.com>
2025-11-15 18:38:27 +0300

    Add AutoMapper profile for LineSubscription

---
+++ HIAST.Transportation.Application/MappingProfiles/LineSubscriptionProfile.cs
@@ -0,0 +1,24 @@
+using AutoMapper;
+using HIAST.Transportation.Application.DTOs.LineSubscription;
+using HIAST.Transportation.Domain.Entities;
+
+namespace HIAST.Transportation.Application.MappingProfiles;
+
+public class LineSubscriptionProfile : Profile
+{
+    public LineSubscriptionProfile()
+    {
+        // Maps for reading data, including nested details.
+        CreateMap<LineSubscription, LineSubscriptionDto>()
+            .ForMember(dest => dest.EmployeeName, opt => opt.MapFrom(src => $"{src.Employee.FirstName} {src.Employee.LastName}"))
+            .ForMember(dest => dest.LineName, opt => opt.MapFrom(src => src.Line.Name));
+            
+        CreateMap<LineSubscription, LineSubscriptionListDto>()
+            .ForMember(dest => dest.EmployeeName, opt => opt.MapFrom(src => $"{src.Employee.FirstName} {src.Employee.LastName}"))
+            .ForMember(dest => dest.LineName, opt => opt.MapFrom(src => src.Line.Name));
+
+        // Maps for writing data.
+        CreateMap<CreateLineSubscriptionDto, LineSubscription>();
+        CreateMap<UpdateLineSubscriptionDto, LineSubscription>();
+    }
+}
