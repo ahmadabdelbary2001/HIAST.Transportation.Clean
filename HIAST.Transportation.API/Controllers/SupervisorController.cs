3caa2975831f97c335b640ce6fa4fd8e4177442e
ahmadabdelbary2001 <ahmadabdelbary2001@gmail.com>
2025-11-26 11:53:33 +0300

    Add and implement SupervisorController

---
+++ HIAST.Transportation.API/Controllers/SupervisorController.cs
@@ -0,0 +1,28 @@
+using HIAST.Transportation.Application.DTOs.Supervisor;
+using HIAST.Transportation.Application.Features.Supervisor.Queries.GetSupervisorLineAssignments;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HIAST.Transportation.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SupervisorController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public SupervisorController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    // GET: api/Supervisor/LineAssignments
+    [HttpGet("LineAssignments")]
+    [ProducesResponseType(typeof(IReadOnlyList<SupervisorLineDto>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<IReadOnlyList<SupervisorLineDto>>> GetLineAssignments()
+    {
+        var query = new GetSupervisorLineAssignmentsQuery();
+        var result = await _mediator.Send(query);
+        return Ok(result);
+    }
+}
